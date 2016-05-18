180dcbad1068f46f697f6199134a29d1e504b9cd
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-05-19 01:29:31 +0530

    init

--- test/mctest_test.exs
+++ test/mctest_test.exs
@@ -2,7 +2,10 @@ defmodule MctestTest do
   use ExUnit.Case
   doctest Mctest
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "encryption" do
+    {:ok, bin } = Mcrypt.block_encrypt "awesome stuff", :rijndael_256, :cbc, String.duplicate("0", 32), String.duplicate("0", 32)
+    IO.inspect bin
+    dec = Mcrypt.block_decrypt bin, :rijndael_256, :cbc, String.duplicate("0", 32), String.duplicate("0", 32)
+    IO.inspect dec
   end
 end
